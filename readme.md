2e9ca56b08f3da56a39349f010dfa31b956df4d4
samAd0san <sohaibsamad.hala@gmail.com>
2024-03-11 16:46:03 +0530

    Added url/path in NodeJS server

--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
-# We are implementing NodeJS server 
+# Implementing NodeJS server 
+### commit 1
 1. node -v
 2. npm init -y
-3. To access the server http://localhost:{port}
+3. import http module
+4. declare a server from http
+5. define the handler function
+6. specify the port
+7. start the server
+8. To access the server http://localhost:3000
+
+### commit 2
+1. same as above, added url/path in the handler function
 
--- server.js
+++ server.js
@@ -1,19 +1,47 @@
-// Import the http module from nodejs library
+// Implementing routes in nodeJS server
 const http = require('http');
 
-// Create a HTTP server using nodejs's built-in http module
-const server = http.createServer(handler); // handler function specified to handle incoming requests.
+const server = http.createServer(handler);
 
-// define the handler function, which handles the incoming request
+// Through the handler function we are adding paths/url in the nodeJS server
 function handler(req,res) {
-    res.write('Welcome to NodeJS');
-    res.end();
+    // req.url in Node.js represents the URL path of the incoming HTTP request.
+    switch(req.url) {
+        // This is home page if no url/path is given
+        case '/':
+        case '/home':
+            res.write('KORENO HOME PAGE DESU');
+            res.end();
+            break;
+
+        case '/books': // This is redirected when given path localhost:3000/books
+            const books = [
+                {id:1, name:'Atomic Habis', price:499},
+                {id:2, name:'RichDad PoorDad',price:399},
+                {id:3, name:'Power of Subconcious Mind',price:299},
+            ];
+
+            res.write(JSON.stringify(books));
+            res.end();
+            break;
+
+        case '/authors': // This is redirected when given path localhost:3000/authors
+            const authors = ['James Clear','David Guetta','Joe Rogan'];
+
+            res.write(JSON.stringify(authors));
+            // res.write(authors); // won't work
+            res.end();
+            break;
+
+        default: // If the url requested does not exist 
+            res.write('Not Found');
+            res.end();
+            break;
+    }
 }
 
-// Specify the port on which we want to host the server
 const port = 3000;
 
-// Start the server to listen for incoming request on the specified port
 server.listen(port,()=>{
-    console.log(`The port is running of ${port}`);
+    console.log(`The port is running on ${port}`);
 })
